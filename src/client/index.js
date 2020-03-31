e815b32d312f1feffb068293d3f88c41497290e6
Niklas Burg <Niklasburg1999.nbgmail.com>
2020-03-31 20:58:39 +0200

    Set up Prod, Fixed IMG issue

--- src/client/index.js
+++ src/client/index.js
@@ -5,6 +5,14 @@ import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
+import {facebook} from "./img/fb.png"
+import {linkedin} from "./img/linkedin.png"
+import {youtube} from "./img/youtube.jpg"
+import {twitter} from "./img/twitter.png"
+import {logo} from "./img/logo.jpg"
+
+
+
 
 
 
